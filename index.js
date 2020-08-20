84ecf07784a09250c77d7be18f775ec61ebe98f8
bhavesh0902 <60576636+bhavesh0902@users.noreply.github.com>
2020-08-20 11:04:04 +0530

    files uploaded

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var diceImage1="images/dice"+randomNumber1+".png";
+
+var x=document.querySelectorAll("img")[0];
+x.setAttribute("src",diceImage1);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var diceImage2="images/dice"+randomNumber2+".png";
+
+var y=document.querySelectorAll("img")[1];
+y.setAttribute("src",diceImage2);
+
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🚩Player 1 wins!!";
+}
+ else if(randomNumber2>randomNumber1)
+ {
+  document.querySelector("h1").innerHTML="Player 2 wins🚩!!";
+ }
+ else
+ {
+   document.querySelector("h1").innerHTML="Draw!";
+ }
